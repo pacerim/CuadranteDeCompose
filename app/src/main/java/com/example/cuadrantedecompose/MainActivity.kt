dddc0062e7d08c472456c1e2d3187e48722d488f
diazpabloo <pdiazdecerio3@gmail.com>
2024-02-20 16:08:36 +0100

    fin

--- app/src/main/java/com/example/cuadrantedecompose/MainActivity.kt
+++ app/src/main/java/com/example/cuadrantedecompose/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.cuadrantedecompose
 
-import android.graphics.Color
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cuadrantedecompose.ui.theme.CuadranteDeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,17 +46,36 @@ class MainActivity : ComponentActivity() {
 fun CuadrantesComposeApp() {
 
     Column(Modifier.fillMaxWidth()) {
-
+        Row(Modifier.weight(1f)) {
+            Cuadrante(
+                header = stringResource(R.string.header1),
+                description = stringResource(R.string.text1),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            Cuadrante(
+                header = stringResource(R.string.header2),
+                description = stringResource(R.string.text2),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            Cuadrante(
+                header = stringResource(R.string.header3),
+                description = stringResource(R.string.text3),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            Cuadrante(
+                header = stringResource(R.string.header4),
+                description = stringResource(R.string.text4),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
     }
-    Cuadrante(
-        stringResource(R.string.header1),
-        stringResource(R.string.text1),
-        stringResource(R.string.header2),
-        stringResource(R.string.text2),
-        stringResource(R.string.header3),
-        stringResource(R.string.text3),
-        stringResource(R.string.header4),
-        stringResource(R.string.text4))
+    
 }
 
 @Composable
@@ -57,6 +85,24 @@ private fun Cuadrante(
     backgroundColor : Color,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = modifier
+            .background(backgroundColor)
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = header,
+            textAlign = TextAlign.Justify,
+            fontWeight = FontWeight.Bold
+            )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+            )
+    }
 
 
 }
